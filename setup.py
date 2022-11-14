38bc5f0ba1992dcac6da6974212cba7a55b28c57
Karl Lorey <git@karllorey.com>
2022-11-14 21:07:17 +0100

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="startupradar-transformers",
+    version="0.1.0",
+    packages=find_packages(include=["startupradar.*"]),
+    install_requires=[
+        "pandas",
+        "scikit-learn",
+        "requests",
+        "black",
+        "tldextract",
+        "flake8",
+        "pytest",
+    ],
+)
