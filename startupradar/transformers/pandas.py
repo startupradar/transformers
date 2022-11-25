7973cfe8ede0487b60a183e3afc62ec9dd74725c
Karl Lorey <git@karllorey.com>
2022-11-25 22:48:27 +0100

    Add OneHotEncoder and ColumnTransformer supporting pandas

--- startupradar/transformers/pandas.py
+++ startupradar/transformers/pandas.py
@@ -3,8 +3,10 @@ sklearn transformers adapted to work on pandas DataFrames or Series'.
 """
 import pandas as pd
 from sklearn.base import TransformerMixin
+from sklearn.compose import ColumnTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.pipeline import FeatureUnion, Pipeline
+from sklearn.preprocessing import OneHotEncoder
 
 
 class FeatureUnionDF(TransformerMixin):
@@ -73,3 +75,53 @@ class TfidfVectorizerDF(Vectorizer):
 class CountVectorizerDF(Vectorizer):
     def __init__(self, **kwargs):
         self.vec = CountVectorizer(**kwargs)
+
+
+class OneHotEncoderDF(TransformerMixin):
+    def __init__(self, **kwargs):
+        kwargs["sparse"] = False
+        self.enc = OneHotEncoder(**kwargs)
+
+    def fit(self, X: pd.DataFrame, y=None):
+        self.enc.fit(X, y)
+        return self
+
+    def transform(self, X: pd.DataFrame):
+        transformed = self.enc.transform(X)
+        columns = self.enc.get_feature_names_out(X.columns)
+        return pd.DataFrame(transformed, columns=columns)
+
+    def get_params(self, deep=False):
+        return self.enc.get_params(deep)
+
+    def set_params(self, **params: dict):
+        return self.enc.set_params(**params)
+
+    def get_feature_names_out(self, features_input=None):
+        return self.enc.get_feature_names_out(features_input)
+
+
+class ColumnTransformerDF(TransformerMixin):
+    def __init__(self, transformers: list, **kwargs):
+        # disable sparse
+        kwargs["sparse_threshold"] = 0
+
+        self.trans = ColumnTransformer(transformers, **kwargs)
+
+    def fit(self, X, y=None):
+        self.trans.fit(X, y)
+        return self
+
+    def transform(self, X):
+        trans = self.trans.transform(X)
+        columns = self.trans.get_feature_names_out(X.columns.values)
+        return pd.DataFrame(trans, columns=columns)
+
+    def get_params(self, deep=False):
+        return self.trans.get_params(deep)
+
+    def set_params(self, **kwargs):
+        return self.trans.set_params(**kwargs)
+
+    def get_feature_names_out(self, input_features=None):
+        return self.trans.get_feature_names_out(input_features)
--- tests/test_pandas.py
+++ tests/test_pandas.py
@@ -1,4 +1,10 @@
-from startupradar.transformers.pandas import TfidfVectorizerDF
+import pandas as pd
+
+from startupradar.transformers.pandas import (
+    TfidfVectorizerDF,
+    OneHotEncoderDF,
+    ColumnTransformerDF,
+)
 
 
 def test_tfidf():
@@ -8,3 +14,26 @@ def test_tfidf():
     for word in " ".join(texts).split():
         word_not_in_cols_msg = f"word not found in columns ({word=}, {out.columns=})"
         assert word in out.columns, word_not_in_cols_msg
+
+
+def test_one_hot_encoder():
+    df = pd.DataFrame([["a"], ["b"], ["c"], ["a"]], columns=["letter"])
+    t = OneHotEncoderDF(handle_unknown="ignore")
+    result = t.fit_transform(df)
+    columns_expected = ["letter_a", "letter_b", "letter_c"]
+    assert result.columns.values.tolist() == columns_expected
+
+    df = pd.DataFrame([["a"], ["b"], ["c"], ["d"]], columns=["letter"])
+    assert t.transform(df).columns.values.tolist() == columns_expected
+
+
+def test_column_transformer():
+    df = pd.DataFrame({"suffix": ["de", "com", "com", "at"], "length": [1, 2, 3, 4]})
+    transformer = ColumnTransformerDF(
+        transformers=[
+            ("one_hot", OneHotEncoderDF(), ["suffix"]),
+            ("pass", "passthrough", ["length"]),
+        ]
+    )
+    df_out = transformer.fit_transform(df)
+    assert len(df_out.columns) == 4
