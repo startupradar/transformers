26fdb61ed46ad89df4346e84095a1d5408e1d0c4
Karl Lorey <git@karllorey.com>
2022-11-29 18:04:26 +0100

    Switch to find_namespace_packages function

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name="startupradar-transformers",
     version="0.1.0",
-    packages=find_packages(include=["startupradar.*"]),
+    packages=find_namespace_packages(include=["startupradar.*"]),
     install_requires=[
         "pandas",
         "scikit-learn",
